d26d40cb4dfe887a4121e4429d98869c5cd07c91
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-23 15:47:36 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <h1 className='w-full  h-full  bg-black text-white'>Hello shushil</h1>
+     
+    </>
+  )
+}
+
+export default App
