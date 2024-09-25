23e4c8a8af1d828fec0516a45081ab1d4660004b
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-25 23:16:22 +0545

    push code

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { IoSearchOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa6";
@@ -7,85 +7,97 @@ import { CiHeart } from "react-icons/ci";
 import { IoReorderThreeOutline } from "react-icons/io5";
 
 const Nav = () => {
-    
+
+    const toggleButton = document.getElementById('toggleButton');
+    const nav = document.getElementById('myNav');
+
+    toggleButton.addEventListener('click', () => {
+        if (nav.style.display === 'none' || nav.style.display === '') {
+            nav.style.display = 'block';  // Show the nav
+        } else {
+            nav.style.display = 'none';  // Hide the nav
+        }
+    });
 
 
-  return (
-    <>
 
-    <div className="w-full h-full py-10 px-[5px]">
-        <div className=" max-w-[1200px] m-auto">
 
-            <div className="w-full h-auto flex justify-between items-center md:justify-end ">
+    return (
+        <>
 
-                <div className="logo">
-                    <h1 className='font-poppins uppercase font-bold text-[23px]'>Swattai</h1>
+            <div className="w-full h-full py-10 px-[5px]">
+                <div className=" max-w-[1200px] m-auto">
+
+                    <div className="w-full h-auto flex justify-between items-center md:justify-end ">
+
+                        <div className="logo">
+                            <h1 className='font-poppins uppercase font-bold text-[23px]'>Swattai</h1>
+                        </div>
+
+                        <ul className='flex gap-[20px] font-poppins md:hidden '>
+
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/contact'>Contact</Link></li>
+                            <li><Link to='/about'>About</Link></li>
+                            <li><Link to='/signup'>Sign Up</Link></li>
+
+
+                        </ul>
+
+
+                        <div className="flex md:justify-end">
+
+                            <div className="input relative  w-[270px] h-[50px] border-[2px]  lg:hidden">
+                                <input type="text" placeholder='What are you looking for ? ' className='w-full h-full bg-[#F5F5F5] pl-[10px] pr-[35px] font-poppins rounded-[2px]' />
+                                <IoSearchOutline className='absolute right-[10px] top-[15px] w-[20px]  h-[20px] color-black' />
+
+
+                            </div>
+
+                            <ul className='flex text-[25px] gap-4 mx-4 py-3 px-2' id='myNav'>
+                                <li ><Link to='/love'>  <CiHeart /></Link></li>
+                                <li ><Link to='/addtocart'> <IoCartOutline /></Link></li>
+                                <li ><Link to='/admin'><FaRegUser className='text-[20px]' /></Link></li>
+                            </ul>
+
+
+
+
+                            <button id='toggleButton'> <IoReorderThreeOutline className='text-[30px] m-[7px] hidden md:block' /> </button>
+
+
+
+
+
+
+
+
+
+
+
+                        </div>
+
+
+
+
+
+
+
+
+                    </div>
+
                 </div>
-
-                <ul className='flex gap-[20px] font-poppins md:hidden '>
-
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to ='/signup'>Sign Up</Link></li>
-                    
-
-                </ul>
-
-
-                <div className="flex md:justify-end">
-
-                <div className="input relative  w-[270px] h-[50px] border-[2px]  lg:hidden">
-                    <input type="text" placeholder='What are you looking for ? ' className='w-full h-full bg-[#F5F5F5] pl-[10px] pr-[35px] font-poppins rounded-[2px]'  />
-                    <IoSearchOutline className='absolute right-[10px] top-[15px] w-[20px]  h-[20px] color-black' />
-
-
-                </div>
-
-                <ul className='flex text-[25px] gap-4 mx-4 py-3 px-2'>
-                    <li ><Link  to='/love'>  <CiHeart /></Link></li>
-                    <li ><Link to='/addtocart'> <IoCartOutline /></Link></li>
-                    <li ><Link to='/admin'><FaRegUser className='text-[20px]' /></Link></li>
-                </ul>
-
-
-
-
-               <button onClick={click()}> <IoReorderThreeOutline className='text-[30px] m-[7px] hidden md:block'/> </button> 
-
-              
-               
-                
-
-
-
-
-
-
-
-                </div>
-
-
-
-
-               
-
 
 
             </div>
 
-        </div>
 
-
-    </div>
-
-
-    <hr className='mb-[70px] bg-black' />
+            <hr className='mb-[70px] bg-black' />
 
 
 
-    </>
-  )
+        </>
+    )
 }
 
 export default Nav
