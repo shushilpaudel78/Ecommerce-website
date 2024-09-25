adc4281f07614732dff610d65a62c87e643e162e
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-25 23:06:38 +0545

    small change in nav responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { IoSearchOutline } from "react-icons/io5";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa6";
 import { CiHeart } from "react-icons/ci";
+import { IoReorderThreeOutline } from "react-icons/io5";
 
 const Nav = () => {
+    
+
+
   return (
     <>
 
     <div className="w-full h-full py-10 px-[5px]">
         <div className=" max-w-[1200px] m-auto">
 
-            <div className="w-full h-auto flex justify-between items-center">
+            <div className="w-full h-auto flex justify-between items-center md:justify-end ">
 
                 <div className="logo">
                     <h1 className='font-poppins uppercase font-bold text-[23px]'>Swattai</h1>
                 </div>
 
-                <ul className='flex gap-[20px] font-poppins '>
+                <ul className='flex gap-[20px] font-poppins md:hidden '>
 
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/contact'>Contact</Link></li>
@@ -29,9 +33,9 @@ const Nav = () => {
                 </ul>
 
 
-                <div className="flex">
+                <div className="flex md:justify-end">
 
-                <div className="input relative  w-[270px] h-[50px] border-[2px] ">
+                <div className="input relative  w-[270px] h-[50px] border-[2px]  lg:hidden">
                     <input type="text" placeholder='What are you looking for ? ' className='w-full h-full bg-[#F5F5F5] pl-[10px] pr-[35px] font-poppins rounded-[2px]'  />
                     <IoSearchOutline className='absolute right-[10px] top-[15px] w-[20px]  h-[20px] color-black' />
 
@@ -43,6 +47,11 @@ const Nav = () => {
                     <li ><Link to='/addtocart'> <IoCartOutline /></Link></li>
                     <li ><Link to='/admin'><FaRegUser className='text-[20px]' /></Link></li>
                 </ul>
+
+
+
+
+               <button onClick={click()}> <IoReorderThreeOutline className='text-[30px] m-[7px] hidden md:block'/> </button> 
 
               
                
