429f61290479da32194999a1d4290af36d5752ec
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-27 14:20:21 +0545

    cart changed

--- src/components/product/Product_viewproduct.jsx
+++ src/components/product/Product_viewproduct.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Product_photo from '/img/product/joystick.png'
 import { FaRegHeart } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
+import { CiStar } from "react-icons/ci";
 
 const Product_viewproduct = () => {
 
@@ -30,20 +31,56 @@ const Product_viewproduct = () => {
                                 </div>
 
 
-                                <div className="w-[190px] h-[180px] ml-10 "><img src={Product_photo} alt="product photo" className='w-full h-full object-cover' /></div>/
+                                <div className="w-[190px] h-[180px] ml-10 "><img src={Product_photo} alt="product photo" className='w-full h-full object-cover' /></div>
 
+                                <button className='w-full py-[10px] text-white bg-black font-poppins text-[16px]'>Add to cart</button>
+
+
+                            </div>
+
+
+                            <div className="flex flex-col p-5 font-poppins text-[16px] gap-2">
+                                <p className='font-semibold'>HAVIT HV-G92 Gamepad</p>
+                                <p className='text-red-500'>$120 <span className='text-[#686666] mx-2 line-through'>$160</span></p>
+                                <div className="flex  text-yellow-500">
+                                    <CiStar />
+                                    <CiStar />
+                                    <CiStar />
+                                    <CiStar />
+                                    <p>(88)</p>
+                                </div>
 
                             </div>
                         </div>
 
 
 
-                        
+
+                        <div className="w-full flex justify-center my-10">
+                            <button className='bg-[#DB4444] text-white py-[10px] px-[20px] rounded-md font-poppins'>View All the product</button>
+                        </div>
 
 
 
 
-                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
