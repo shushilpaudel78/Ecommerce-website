e82f96839b0b5a51b59f6ef6c90b22a7888e438c
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-28 21:26:26 +0545

    push catagories

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,9 +7,7 @@ const Home = () => {
     <>
 
     <Product  />
-
-
-          
+ 
     </>
   )
 }
--- src/components/product/Product_ours.jsx
+++ src/components/product/Product_ours.jsx
@@ -147,7 +147,7 @@ const Product_ours = () => {
 
 
 
-                        
+
 
                         <div className="w-[25%] ">
 
@@ -178,17 +178,17 @@ const Product_ours = () => {
                                 <p className='font-semibold'>ASUS FHD Gaming Laptop</p>
                                 <div className="flex gap-4">
 
-                                <p className='text-red-500'>$700 </p>
-                                <div className="flex  text-yellow-500">
-                                    <CiStar />
-                                    <CiStar />
-                                    <CiStar />
-                                    <CiStar />
-                                    <p>(325)</p>
-                                </div>
+                                    <p className='text-red-500'>$700 </p>
+                                    <div className="flex  text-yellow-500">
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <p>(325)</p>
+                                    </div>
 
                                 </div>
-                              
+
 
                             </div>
                         </div>
@@ -199,7 +199,7 @@ const Product_ours = () => {
 
 
 
-                        
+
 
                         <div className="w-[25%] ">
 
@@ -230,20 +230,144 @@ const Product_ours = () => {
                                 <p className='font-semibold'>ASUS FHD Gaming Laptop</p>
                                 <div className="flex gap-4">
 
-                                <p className='text-red-500'>$700 </p>
-                                <div className="flex  text-yellow-500">
-                                    <CiStar />
-                                    <CiStar />
-                                    <CiStar />
-                                    <CiStar />
-                                    <p>(325)</p>
-                                </div>
+                                    <p className='text-red-500'>$700 </p>
+                                    <div className="flex  text-yellow-500">
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <p>(325)</p>
+                                    </div>
 
                                 </div>
-                              
+
 
                             </div>
                         </div>
+
+
+
+
+
+
+
+
+
+                        {/* Radio Button Section */}
+
+
+                        <div className="w-[25%] ">
+
+                            <div className="bg-[#F5F5F5] mx-3 my-3">
+
+
+                                <div className="flex justify-between px-6 py-5">
+
+                                    <p className='text-[16px] font-poppins bg-[#00FF66] w-[80px] h-[40px] text-center pt-[8px] text-white'>New</p>
+
+                                    <div className="flex flex-col gap-4 text-[30px] left ">
+                                        <FaRegHeart />
+                                        <IoEyeSharp />
+
+                                    </div>
+
+
+                                </div>
+
+
+                                <div className="w-[190px] h-[180px] ml-10 "><img src={comppic} alt="product photo" className='w-full h-full object-cover' /></div>
+
+                                <button className='w-full py-[10px] text-white bg-black font-poppins text-[16px]'>Add to cart</button>
+
+
+                            </div>
+
+
+                            <div className="flex flex-col p-5 font-poppins text-[16px] gap-2">
+                                <p className='font-semibold'>ASUS FHD Gaming Laptop</p>
+                                <div className="flex gap-4">
+
+                                    <p className='text-red-500'>$700 </p>
+                                    <div className="flex  text-yellow-500">
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <p>(325)</p>
+                                    </div>
+
+                                </div>
+                                <div className="flex gap-3">
+                                    <input type="radio" name='product' className='bg-red-500' />
+                                    <input type="radio" name='product' className='bg-red-500' />
+
+
+
+                                </div>
+
+
+
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div className="w-[25%] ">
+
+                            <div className="bg-[#F5F5F5] mx-3 my-3">
+
+
+                                <div className="flex justify-between px-6 py-5">
+
+                                    <p className='text-[16px] font-poppins bg-[#00FF66] w-[80px] h-[40px] text-center pt-[8px] text-white'>New</p>
+
+                                    <div className="flex flex-col gap-4 text-[30px] left ">
+                                        <FaRegHeart />
+                                        <IoEyeSharp />
+
+                                    </div>
+
+
+                                </div>
+
+
+                                <div className="w-[190px] h-[180px] ml-10 "><img src={comppic} alt="product photo" className='w-full h-full object-cover' /></div>
+
+                                <button className='w-full py-[10px] text-white bg-black font-poppins text-[16px]'>Add to cart</button>
+
+
+                            </div>
+
+
+                            <div className="flex flex-col p-5 font-poppins text-[16px] gap-2">
+                                <p className='font-semibold'>ASUS FHD Gaming Laptop</p>
+                                <div className="flex gap-4">
+
+                                    <p className='text-red-500'>$700 </p>
+                                    <div className="flex  text-yellow-500">
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <CiStar />
+                                        <p>(325)</p>
+                                    </div>
+
+                                </div>
+                                <div className="flex gap-3">
+                                    <input type="radio" name='product' className='bg-red-500' />
+                                    <input type="radio" name='product' className='bg-red-500' />
+
+
+
+                                </div>
+
+
+
+                            </div>
+                        </div>
+
 
 
 
