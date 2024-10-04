bb46ed22e43dc8ee5d1fe1506758625bf9ea80d1
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-10-04 17:06:18 +0545

    change little

--- src/components/profile/Edit_profile.jsx
+++ src/components/profile/Edit_profile.jsx
@@ -3,39 +3,39 @@ import React from 'react'
 const Edit_profile = () => {
     return (
         <>
-            <div className="w-full h-auto">
+            <div className="w-full h-auto shadow-lg my-2 px-10 py-10 ">
 
                 <h1 className='text-[#DB4444] text-[20px] font-normal font-poppins'>Edit Your Profile</h1>
 
                 <div className="flex flex-wrap font-poppins text-[16px] font-normal">
 
-                    <div className="flex w-[50%] flex-col my-6">
+                    <div className="flex w-[50%] flex-col my-6 pr-4">
                         <p>Firstname</p>
-                        <input type="text" className='bg-[#F5F5F5]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
                     </div>
 
                     <div className="flex w-[50%] flex-col my-6">
                         <p>Lastname</p>
-                        <input type="text" className='bg-[#F5F5F5]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
                     </div>
 
-                    <div className="flex w-[50%] flex-col my-6">
+                    <div className="flex w-[50%] flex-col my-6 pr-4">
                         <p>Email</p>
-                        <input type="text" className='bg-[#F5F5F5]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
                     </div>
 
                     <div className="flex w-[50%]  flex-col my-6">
                         <p>Address</p>
-                        <input type="text"  className='bg-[#F5F5F5]'/>
+                        <input type="text"  className='bg-[#F5F5F5] h-[50px]'/>
                     </div>
 
 
                     <div className="flex w-full  flex-col my-6 gap-4">
 
                         <h1 className='w-full'>Password Change</h1>
-                        <input type="text" className='bg-[#F5F5F5]' />
-                        <input type="text" className='bg-[#F5F5F5]' />
-                        <input type="text" className='bg-[#F5F5F5]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
+                        <input type="text" className='bg-[#F5F5F5] h-[50px]' />
 
 
                     </div>
--- src/components/profile/Profile_sidebar.jsx
+++ src/components/profile/Profile_sidebar.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const Profile_sidebar = () => {
     return (
         <>
-            <div className="flex flex-col font-poppins text-[16px] font-normal">
+            <div className="flex flex-col font-poppins text-[16px] font-normal gap-4 my-4">
 
                 <p>Manage My Account</p>
 
 
-                <ul>
+                <ul className='px-6 text-[#777272]'>
                     <li><Link>My Profile</Link></li>
                     <li><Link>Address Book</Link></li>
                     <li><Link>My Payment Options</Link></li>
@@ -23,7 +23,7 @@ const Profile_sidebar = () => {
                 <p>My Orders</p>
 
 
-                <ul>
+                <ul className='px-6 text-[#777272]'>
                     <li><Link>My Return</Link></li>
                     <li><Link>My Concelation</Link></li>
 
